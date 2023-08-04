832b9989eb57b66e13cfb42e7139cb1a90bca0ad
Mateus Schiavi <mtsschiavi@gmail.com>
2023-08-04 13:28:40 -0300

    MINI PROYECTO

---
+++ README.md
@@ -0,0 +1,24 @@
+
+Bienvenido al MiniProyecto del nivel 1. En este proyecto estaremos aplicando los conocimientos adquiridos durante esta primera y segunda semana. Sigue las instrucciones de este archivo para completar el proyecto y ten en cuenta que estas mismas instrucciones se tomarán en cuenta para la evaluación del proyecto.
+
+# Instrucciones
+
+En este repositorio encontrarás Los archivos necesarios para construir una aplicación para calcular las propinas de un mozo/mesero. Sin embargo el proyecto que haz recibido aún no es funcional, tu tarea será completar el proyecto para que funcione correctamente.
+
+## Requerimientos
+
+Para completar este proyecto necesitarás:
+- Descargar los archivos de deste repositorio, para esto puedes ir al boton superior que dice "code" y seleccionar la opcion "Download ZIP".
+- Lee detalladamente los archivos JavaScript que existen dentro de este programa, para esto puedes abrirlos con un editor de texto como Visual Studio Code.
+- Completa el código faltante en los archivos JavaScript para que el programa funcione correctamente.
+
+## Criterios de Evaluación
+
+- La función `runProgram()` debe ser llamada al final del archivo `main.js` para que el programa funcione correctamente. Tiene el objetivo de ejecutar las acciones cada vez que se va modificando los datos.
+- la función `reset()` debe ser llamada al final del archivo `main.js` para que el programa funcione correctamente. Tiene el objetivo de reiniciar los valores de las entradas del usuario.
+- la función `calcularTip()` tiene como objetivo calcular el monto de propina a pagar por cada persona.
+- la función `calcularTotal()` tiene como objetivo calcular el monto total a pagar por cada persona incluyendola propina.
+- la función `calcularTodo()` tiene como objetivo ejecutar las dos funciones anteriores.
+
+- El campo "tip amount" indica el monto de propina que equivale para una persona.
+- El campo "Total" indica el monto total a pagar por una persona incluyendo la propina.
---
+++ js/utilities.js
@@ -0,0 +1,73 @@
+/**
+ * Esta función te permite hacer un reset de los campos selecciónados por el usuario.
+ * No se retorna nada. recuerca que algunos valores se hace el reset a 0, otros a 1
+ */
+export function reset(billInput, customPercentageBtn, peopleInput) {
+    billInput.value = "0";
+    peopleInput.value = "1";
+
+
+    customPercentageBtn.forEach(button => {
+        button.value = "";
+    });
+}
+
+/**
+ * Esta función te permite calcular el tip(propina) que se recibirá
+ * por cada persona.
+ * @returns el monto de tip por persona.
+ */
+export function calcularTip(bill, tip, people) {
+
+    const totalBill = (bill * tip / 100);
+    const tipPerPerson = totalBill / people;
+    return tipPerPerson.toFixed(2);
+}
+
+/**
+ * Calcula el total a pagar por cada persona, incluyendo el tip que
+ * le corresponde.
+ * @returns el monto total por persona.
+ */
+export function calcularTotal(bill, tip, people) {
+
+    const totalTip = (bill * tip) / 100;
+    const totalPerPerson = (bill + totalTip) / people;
+    return totalPerPerson.toFixed(2);
+}
+
+/**
+ * Ejecutar tanto la función calcularTip() y la función 
+ * calcularTotal(), luego retornar un array, teniendo en la posición 0 
+ * el retorno de calcularTip() y en la posición 1 el retorno de calcularTotal()
+ * @returns array [] con dos valores.
+ */
+export function calcularTodo(bill, tip, people) {
+    let a = calcularTip(bill, tip, people);
+    let b = calcularTotal(bill, tip, people);
+
+    return [a, b];
+}
+
+/**
+ * Valida si el valor de un campo es 0 o no, si lo es retorna true, si no, retorna false
+ */
+export function esCero(input) {
+
+    switch (input) {
+        case 0:
+            return true;
+        default:
+            return false;
+
+    }
+}
+
+/**
+ * Añade la clase "active" al elemento.
+ * @param { Node } input - un elemento input de html.
+ * puedes investigar sobre : uso de classList en los elementos DOM. añadir, remover.
+ */
+export function claseActivo(input) {
+    input.classList.add("active");
+}
